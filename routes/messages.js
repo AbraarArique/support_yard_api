92ab05927fdc6e7c4c28490c70b3ab6fde7f8ad3
Abraar Arique Diganto <abraararique@icloud.com>
2020-05-03 17:06:21 +0600

    Update getMessage

--- routes/messages.js
+++ routes/messages.js
@@ -20,7 +20,11 @@ const create = async (req, res) => {
 
 const update = async (req, res) => {
   try {
-    const message = await getMessage(req.user, req.params.messageId);
+    const message = await getMessage(
+      req.user,
+      req.params.ticketId,
+      req.params.messageId
+    );
     updateDoc(message, req.body, whitelist);
     await message.save();
     res.status(200).json(message);
@@ -31,7 +35,11 @@ const update = async (req, res) => {
 
 const destroy = async (req, res) => {
   try {
-    const message = await Message.deleteOne({ userId: req.user._id, _id: req.params.messageId });
+    const message = await Message.deleteOne({
+      userId: req.user._id,
+      ticketId: req.params.ticketId,
+      _id: req.params.messageId
+    });
     if (message.n < 1) throw new Error('Operation not permitted.');
     res.status(200).json({});
   } catch (e) {
@@ -39,8 +47,8 @@ const destroy = async (req, res) => {
   }
 }
 
-const getMessage = async (user, id) => {
-  return await Message.findOne({ userId: user._id, _id: id });
+const getMessage = async (user, ticketId, id) => {
+  return await Message.findOne({ userId: user._id, ticketId: ticketId, _id: id });
 }
 
 const whitelist = [
